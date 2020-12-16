bad1dbbd2711287bc050fcb29621e8196f1b2303
suarasiy <sidiqindrajatiyusuf@gmail.com>
2020-12-17 03:53:20 +0800

    Test setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="tenorscrap",
+    version="0.0.1",
+    author="siy",
+    author_email="",
+    description="Search gif in Tenor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/suarasiy/tenorscrap",
+    install_requires=["beautifulsoup4", "requests"]
+)
